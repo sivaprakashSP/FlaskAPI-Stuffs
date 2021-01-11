ec6d4f54ab639f01ca18b5a0ebae46b37b7c2f4c
sivaprakashSP <sivaprakashsp@gmail.com>
2021-01-11 14:28:29 +0530

    API Created using Flask

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 import flask
 import random
-from flask import request,jsonify
-import json 
+#from flask import request,jsonify
+#import json 
 
 app=flask.Flask(__name__)
 app.config["DEBUG"]=True
@@ -10,43 +10,83 @@ app.config["DEBUG"]=True
 def page():
     return "API for collecting datasets"
 
-data=[
-    {
-        'temparture':36.7,
-        'pressure':110/40,
-        'glucose':150,
-        'heartrate':80,
-        'oxygensaturation':96,
-        'electrocardiogram':150,
-    },
-    {
-        'temparture':40.1,
-        'pressure':130/40,
-        'glucose':250,
-        'heartrate':50,
-        'oxygensaturation':94,
-        'electrocardiogram':170,
-    },
-    {
-        'temparture':50.1,
-        'pressure':150/40,
-        'glucose':400,
-        'heartrate':90,
-        'oxygensaturation':105,
-        'electrocardiogram':215,
-    },
-    {
-        'temparture':35.1,
-        'pressure':120/40,
-        'glucose':300,
-        'heartrate':98,
-        'oxygensaturation':92,
-        'electrocardiogram':150,
-    },
-]
+@app.route('/api/normal',methods=['GET'])
+def normal():
+    data={
+        "temparture":random.randint(36,37),
+        "pressure":random.randint(90,120),
+        "respiration":random.randint(12,16),
+        "glucose":random.randint(72,140),
+        "heartrate":random.randint(60,100),
+        "oxygenSaturation":random.randint(95,100),
+        "cholestrol":random.randint(125,200)
+    }
+    return(data)
 
-@app.route('/api/datasets',methods=['GET'])
-def datasets():
-    if request.method=="GET": 
-        return jsonify(data[0:2]) 
-app.run()
+@app.route('/api/diabetes',methods=['GET'])
+def diabetes():
+    dataset={
+        "temperature": random.randint(35, 37),
+        "pressure": random.randint(90, 120),
+        "respiration": random.randint(12, 16),
+        "glucose": random.randint(72, 140),
+        "heartRate": random.randint(60, 100),
+        "oxygenSaturation": random.randint(50, 96),
+        "cholesterol": random.randint(125, 200)
+    }
+    return(dataset)
+
+@app.route('/api/prediabetes',methods=['GET'])
+def prediabetes():
+    dataset={
+        "temperature": random.randint(36, 38),
+        "pressure": random.randint(90, 120),
+        "respiration": random.randint(12, 16),
+        "glucose": random.randint(140,199),
+        "heartRate": random.randint(60, 100),
+        "oxygenSaturation": random.randint(95,100),
+        "cholesterol": random.randint(125, 200)
+
+    }
+    return(dataset)
+
+@app.route('/api/ModerateAcuteAsthma',methods=['GET'])
+def ModerateAcuteAsthma():
+    dataset={
+
+        "Temperature": random.randint(35, 37),
+        "Pressure": random.randint(90, 120),
+        "respiration": random.randint(20, 30),
+        "glucose": random.randint(72, 140),
+        "heartRate": random.randint(100, 125),
+        "oxygenSaturation": random.randint(92, 95),
+        "cholesterol": random.randint(125, 200),
+    }
+    return(dataset)
+
+@app.route('/api/Hypoxemia',methods=['GET'])
+def hypoxemia():
+    dataset={
+        "Temperature": random.randint(35, 37),
+        "Pressure": random.randint(90, 120),
+        "respiration": random.randint(20, 30),
+        "glucose": random.randint(72, 140),
+        "heartRate": random.randint(100, 125),
+        "oxygenSaturation": random.randint(92, 95),
+        "cholesterol": random.randint(125, 200),
+    }
+    return(dataset)
+
+@app.route('/api/Bronchiectasis',methods=['GET'])
+def bronchiectasis():
+    dataset={
+        "Temperature": random.randint(35, 37),
+        "Pressure": random.randint(90, 120),
+        "respiration": random.randint(40, 60),
+        "glucose": random.randint(72, 140),
+        "heartRate": random.randint(60, 100),
+        "oxygenSaturation": random.randint(95, 100),
+        "cholesterol": random.randint(125, 200),
+    }
+    return(dataset)
+app.run(debug=True)
